b4600e56d9089970903c6ed72a04a3b4736529c4
kaviyarsan2004 <kavi580580@gmail.com>
2024-02-04 12:44:37 -0500

    added login, logout, signup pages

--- website/views.py
+++ website/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    # Check is the user loging in
+    if request.method=='POST':
+        username=request.POST['User_name']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Login successfull")
+            return redirect('home')
+        else:
+           messages.success(request,"Error...")
+           return redirect('home')
+    else:
+        return render(request,'home.html',{})
+
+def User_logout(request):
+     logout(request)
+     messages.success(request,"You have been logout")
+     return redirect('home')
+
+def User_register(request):
+  return render(request,'register.html',{})
